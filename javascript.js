2e46db773c0bcaa5ed7c711497c20a527ecf3c4a
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-09 11:05:11 -0500

    feat: Add computed div with hover

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let divGrid = document.querySelector('.grid-container');
 let div = document.createElement('div');
 
 div.className = 'grid-item';
-
+div.style.minWidth = divGrid.offsetWidth + 'px';
 
 function sketchGridItem(event){
     event.target.style.backgroundColor = 'black';
