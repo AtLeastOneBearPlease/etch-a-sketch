cd93177d2055b1342e9ac6721ef6a68293aa4af1
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-09 11:05:11 -0500

    feat: Add event delegation to grid container

--- javascript.js
+++ javascript.js
@@ -9,10 +9,13 @@ for(let i = 0; i < gridSize * gridSize; i++){
     let newDiv = document.createElement('div');
     newDiv.className = 'grid-item';
     newDiv.style.minWidth = divWidth + 'px'; //minWidth so that it is forced to wrap and make grid
-    newDiv.addEventListener('mouseover', sketchGridItem);
     divGrid.appendChild(newDiv);
 }
 
+divGrid.addEventListener('mouseover', sketchGridItem);
+
 function sketchGridItem(event){
-    event.target.style.backgroundColor = 'black';
+    if(event.target.className === 'grid-item'){
+        event.target.style.backgroundColor = 'black';
+    }
 }
