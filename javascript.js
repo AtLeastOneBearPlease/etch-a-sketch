d8cf29cdb11bbe080b90ca399ba1bd9a731e25d5
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-09 11:05:11 -0500

    feat: Add generateNewDivGrid() for reusability
    The function takes in the length of one side of the grid
    and generated a grid of divs that width and height. It uses
    a document fragment to avoid too many DOM manipulations

--- javascript.js
+++ javascript.js
@@ -1,21 +1,26 @@
-let gridSize = 2; //size of one side of grid
+let gridContainer = document.querySelector('.grid-container');
 
-let divGrid = document.querySelector('.grid-container');
+gridContainer.appendChild(generateNewDivGrid(16));
 
-//make sure 
-let divWidth = divGrid.offsetWidth / gridSize;
-
-for(let i = 0; i < gridSize * gridSize; i++){
-    let newDiv = document.createElement('div');
-    newDiv.className = 'grid-item';
-    newDiv.style.minWidth = divWidth + 'px'; //minWidth so that it is forced to wrap and make grid
-    divGrid.appendChild(newDiv);
-}
-
-divGrid.addEventListener('mouseover', sketchGridItem);
+gridContainer.addEventListener('mouseover', sketchGridItem);
 
 function sketchGridItem(event){
     if(event.target.className === 'grid-item'){
         event.target.style.backgroundColor = 'black';
     }
+}
+
+//generates new div grid, place inside doc fragment, and returns the fragment
+function generateNewDivGrid(gridSize){
+    let divWidth = gridContainer.offsetWidth / gridSize; 
+    let docFragment = document.createDocumentFragment();
+
+    for(let i = 0; i < gridSize * gridSize; i++){
+        let newDiv = document.createElement('div');
+        newDiv.className = 'grid-item';
+        newDiv.style.minWidth = divWidth + 'px'; //minWidth to force wrap for grid
+        docFragment.appendChild(newDiv);
+    }
+
+    return docFragment;
 }
